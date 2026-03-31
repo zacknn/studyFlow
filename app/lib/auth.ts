150270d677927270e02236663e9ba3eb89aa5a95
zaknx7 <zakitalhi13@gmail.com>
2026-04-01 00:48:30 +0100

    fix(auth) : add resend, trying to add forget pass and virification emails

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,22 +1,40 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
+import { resend } from "./resend";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
-    emailAndPassword:{
-       enabled:true ,
-       autoSignIn: false,
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+    sendResetPassword: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: user.email,
+        subject: "Reset your password",
+        html: `<p>Click <a href="${url}">here</a> to reset your password.</p>`,
+      });
     },
-    socialProviders:{
-        github: {
-            clientId: process.env.GITHUB_CLIENT_ID!,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-        }
-
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: user.email,
+        subject: "Verify your email",
+        html: `<p>Click <a href="${url}">here</a> to verify your account.</p>`,
+      });
     },
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
-    
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
 });
--- app/lib/uploadthing.ts
+++ app/lib/uploadthing.ts
@@ -1,7 +1,5 @@
-import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react"
-import type { FileUpload } from "../api/uploadthing/cors"
-import { generateReactHelpers } from "@uploadthing/react"
+import type { FileUpload } from "../api/uploadthing/cors";
+import { generateReactHelpers } from "@uploadthing/react";
 
-export const UploadButton = generateUploadButton<FileUpload>()
-export const UploadDropzone = generateUploadDropzone<FileUpload>()
-export const { useUploadThing, uploadFiles } = generateReactHelpers<FileUpload>()
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<FileUpload>();
