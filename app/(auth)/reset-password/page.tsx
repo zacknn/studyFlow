6c0d56ba3944c244419c5ea0fe1c6860a8245a0c
zaknx7 <zakitalhi13@gmail.com>
2026-06-05 15:38:13 +0100

    fix(reset-password) : fix the rest password page render essus

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,10 +1,12 @@
 import { ResetPasswordForm } from "@/components/reset-password-form"
-
+import { Suspense } from "react"
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <ResetPasswordForm />
+        <Suspense fallback={<div className="text-center">Loading...</div>}>
+          <ResetPasswordForm />
+        </Suspense>
       </div>
     </div>
   )
